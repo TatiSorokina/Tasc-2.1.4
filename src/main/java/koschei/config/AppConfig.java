355114aec29418ebb13881761a9a5e900c2bd23b
TaniaSorokina <tania_sorokina@mail.ru>
2024-01-27 17:43:56 +0300

    Решение задачи Tasc-2.1.4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,10 +1,13 @@
 package koschei.config;
 
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
@@ -13,5 +16,9 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+    @Bean
+    public static Egg6 getEgg(Needle7 needle){
+        return new Egg6(needle);
     }
 }
