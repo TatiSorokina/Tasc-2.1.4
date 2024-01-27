0f60cb85aceeea7927394994b15a5da91da086ca
TaniaSorokina <tania_sorokina@mail.ru>
2024-01-27 17:46:52 +0300

    Решение задачи Tasc-2.1.4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -7,7 +7,6 @@ import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
